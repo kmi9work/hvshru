57908547e814a9153137baa0b75e1853d505f845
Михаил Костенчук <MichaelKostekchuk@iMac-iMac.local>
2011-03-12 22:18:46 +0300

    12.03 1st and 2nd numbers 2011

--- pages/summary.php
+++ pages/summary.php
@@ -1,10 +1,17 @@
-<b>2010 № 1:</b><br> <a href="index.php?p=01_2010_rus">Русская версия</a>, <a href="index.php?p=01_2010_eng">English version</a><br>
-<b>2010 № 2:</b><br> <a href="index.php?p=02_2010_rus">Русская версия</a>, <a href="index.php?p=02_2010_eng">English version</a><br>
-<b>2010 № 3:</b><br> <a href="index.php?p=03_2010_rus">Русская версия</a>, <a href="index.php?p=03_2010_eng">English version</a><br>
-<b>2010 № 4:</b><br> <a href="index.php?p=04_2010_rus">Русская версия</a>, <a href="index.php?p=04_2010_eng">English version</a><br>
-<b>2010 № 5:</b><br> <a href="index.php?p=05_2010_rus">Русская версия</a>, <a href="index.php?p=05_2010_eng">English version</a><br>
-<b>2010 № 6:</b><br> <a href="index.php?p=06_2010_rus">Русская версия</a>, <a href="index.php?p=06_2010_eng">English version</a><br>
-<b>2010 № 7:</b><br> <a href="index.php?p=07_2010_rus">Русская версия</a>, <a href="index.php?p=07_2010_eng">English version</a><br>
-<b>2010 № 8:</b><br> <a href="index.php?p=08_2010_rus">Русская версия</a>, <a href="index.php?p=08_2010_eng">English version</a><br>
-<b>2010 № 9:</b><br> <a href="index.php?p=09_2010_rus">Русская версия</a>, <a href="index.php?p=09_2010_eng">English version</a><br>
+<h1>Аннотации статей</h1>
+<h2>2011</h2>
+<b>2011 № 2:</b><br> <a href="index.php?p=02_2011_rus">Русская версия</a>, <a href="index.php?p=02_2011_eng">English version</a><br>
+<b>2011 № 1:</b><br> <a href="index.php?p=01_2011_rus">Русская версия</a>, <a href="index.php?p=01_2011_eng">English version</a><br>
+<h2>2010</h2>
 <b>2010 № 10:</b><br> <a href="index.php?p=10_2010_rus">Русская версия</a>, <a href="index.php?p=10_2010_eng">English version</a><br>
+<b>2010 № 9:</b><br> <a href="index.php?p=09_2010_rus">Русская версия</a>, <a href="index.php?p=09_2010_eng">English version</a><br>
+<b>2010 № 8:</b><br> <a href="index.php?p=08_2010_rus">Русская версия</a>, <a href="index.php?p=08_2010_eng">English version</a><br>
+<b>2010 № 7:</b><br> <a href="index.php?p=07_2010_rus">Русская версия</a>, <a href="index.php?p=07_2010_eng">English version</a><br>
+<b>2010 № 6:</b><br> <a href="index.php?p=06_2010_rus">Русская версия</a>, <a href="index.php?p=06_2010_eng">English version</a><br>
+<b>2010 № 5:</b><br> <a href="index.php?p=05_2010_rus">Русская версия</a>, <a href="index.php?p=05_2010_eng">English version</a><br>
+<b>2010 № 4:</b><br> <a href="index.php?p=04_2010_rus">Русская версия</a>, <a href="index.php?p=04_2010_eng">English version</a><br>
+<b>2010 № 3:</b><br> <a href="index.php?p=03_2010_rus">Русская версия</a>, <a href="index.php?p=03_2010_eng">English version</a><br>
+<b>2010 № 2:</b><br> <a href="index.php?p=02_2010_rus">Русская версия</a>, <a href="index.php?p=02_2010_eng">English version</a><br>
+<b>2010 № 1:</b><br> <a href="index.php?p=01_2010_rus">Русская версия</a>, <a href="index.php?p=01_2010_eng">English version</a><br>
+
+<br>
